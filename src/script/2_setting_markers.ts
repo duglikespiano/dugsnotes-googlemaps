9daa5caf33825ac8aca0e943a8d64141cb69f61c
SUHO PARK <duglikespiano@gmail.com>
2024-11-04 02:30:34 +0900

    2 - SETTING MARKERS

--- src/script/2_setting_markers.ts
+++ src/script/2_setting_markers.ts
@@ -1,4 +1,5 @@
 const GOOGLEMAPS_API_KEY = import.meta.env.PUBLIC_GOOGLEMAPS_API_KEY as string;
+const GOOGLEMAPS_MAP_KEY = import.meta.env.PUBLIC_GOOGLEMAPS_MAP_KEY as string;
 const mapElement = document.querySelector('#map');
 
 declare global {
@@ -10,7 +11,8 @@ declare global {
 export function loadGoogleMaps(): Promise<google.maps.Map> {
 	return new Promise((resolve, reject) => {
 		const script = document.createElement('script');
-		script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLEMAPS_API_KEY}&callback=initMap`;
+		script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLEMAPS_API_KEY}&libraries=marker&callback=initMap`;
+
 		script.async = true;
 		script.onerror = () => reject('Google Maps failed to load');
 		document.head.appendChild(script);
@@ -19,8 +21,15 @@ export function loadGoogleMaps(): Promise<google.maps.Map> {
 			const mapElement = document.getElementById('map');
 			if (mapElement) {
 				const map = new google.maps.Map(mapElement, {
-					center: { lat: 30.035781, lng: 31.238962 },
+					center: { lat: 48.858222, lng: 2.2945 },
 					zoom: 12,
+					mapId: GOOGLEMAPS_MAP_KEY,
+				});
+
+				const advancedMarker = new google.maps.marker.AdvancedMarkerElement({
+					map,
+					position: { lat: 48.858222, lng: 2.2945 },
+					title: 'The Eiffel Tower',
 				});
 
 				resolve(map);
